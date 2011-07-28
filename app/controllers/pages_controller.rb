5cae525b779da019d9ee902adcc068cea086f3f8
Adam Jones <adam.jones@adamjonesproductions.com>
2011-07-28 16:22:15 -0400

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,17 @@
 class PagesController < ApplicationController
   def home
+	@title = "Home"
+	@testdata = "Here is an example of dynamic test data for the Home page."
   end
 
   def contact
+	@title = "Contact"
+	@testdata = "Here I would insert contact data and links."
+  end
+  
+  def about
+	@title = "About"
+	@testdata = "Here I would extole my own virutes at great length."
   end
 
 end
